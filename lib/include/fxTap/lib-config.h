e1b89fbf869f66a1ec85160c6ec58b9ae9d2f717
陈湛明 <chenzhanming04@foxmail.com>
2024-10-06 20:27:42 +0000

    Stop auto detecting environment

--- lib/include/fxTap/lib-config.h
+++ lib/include/fxTap/lib-config.h
@@ -8,23 +8,16 @@
 
 // If you're not sure, pick ONLY ONE macro below, and we'll get it sorted.
 
-#define FXTAP_CORE_AUTO_DETECT_ENVIRONMENT
-//#define FXTAP_CORE_ON_GINT
-//#define FXTAP_CORE_ON_ARDUINO
-//#define FXTAP_CORE_ON_MODERN_OS
+// * FXTAP_CORE_ON_GINT
+// * FXTAP_CORE_ON_ARDUINO
+// * FXTAP_CORE_ON_MODERN_OS
 
 // Please DO NOT touch the code below.
 
-#if defined(FXTAP_CORE_AUTO_DETECT_ENVIRONMENT)
-
-    #if defined(_WIN32) | defined(_WIN64) | defined(__APPLE__) | defined(__linux__)
-        #define FXTAP_CORE_ON_MODERN_OS
-    #endif
-
-    #if defined(FX9860G) | defined(FXCG50)
-        #define FXTAP_CORE_ON_GINT
-    #endif
-
+#if !defined(FXTAP_CORE_ON_GINT)      & \
+    !defined(FXTAP_CORE_ON_ARDUINO)   & \
+    !defined(FXTAP_CORE_ON_MODERN_OS)
+    #error Platform for fxTap Core not selected.
 #endif
 
 #if defined(FXTAP_CORE_ON_GINT) | defined(FXTAP_CORE_ON_MODERN_OS)
